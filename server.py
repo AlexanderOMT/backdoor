82e026301c88083302b197bef08b0510834b7435
AlexanderOMT <oscar_martin2001@hotmail.com>
2022-12-23 21:26:45 +0100

    v0.2 >> Server communication improved

--- server.py
+++ server.py
@@ -3,72 +3,75 @@ import multiprocessing
 import threading
 import sys
 from threading import Event
-from stoppable_thread import StoppableThread
+
 import queue
+import time
 
 class Server:
     def __init__(self, host, port):
         self.host, self.port = host, port
         self.EXIT_COMMAND = 'exit'
+        self.time_out = 1.0
+        self.buffer = str()
         self.threads = list()
-        self.server = socket.socket()
-        self.server.bind( (host, port) )
+        self.handle_io_lock = threading.Lock()
+        self.server = socket.create_server(
+            (host, port),
+        )
     
-    def _handle_io(self, reading, read = None, write = None):
-        sys.stdout.write('\x1b[2K')
+    def _handle_io(self, reading, write = None, read = None):
+
+        sys.stdout.write('\x1b[2K') # clear cursor line
         if not reading:
-            sys.stdout.write(f'\r[*] {self.host}:{self.port} >> {write}')
-            pass
+            sys.stdout.write(f'\r[*] {self.host}:{self.port} >> ')
+            return input() 
         else:
             sys.stdout.write(f'\r[+] Server read: {read} \n')
-
-    def read_cli(self, stop_threads, cli_queue):
-        # TODO this function shouldnt be here   
+    
+    def _read_client(self, client_socket):
+        client_socket.settimeout(self.time_out)
         while True:
-            read_out = input()
-            cli_queue.put(read_out)
-            if stop_threads():
-                break
-
-    def _read_client(self, stop_threads, client_socket):
-        while True:
-            read = client_socket.recv(1024)
-            if read:
+            try:
+                read = client_socket.recv(4096)
+                self.handle_io_lock.acquire()
                 self._handle_io(reading=True, read=read)
-            if stop_threads():
-                break
+                self.handle_io_lock.release()
+            except socket.timeout:
+                pass
 
-    def _write_client(self, stop_threads, client_socket):
+            if self.buffer == self.EXIT_COMMAND:
+                    break
+
+    def _write_client(self, client_socket):
+
         cli_queue = queue.Queue()
 
-        read_cli_thread = threading.Thread(target = self.read_cli, args=(stop_threads, cli_queue,))
-        self.threads.append(read_cli_thread)
-        read_cli_thread.start()
-
         while True:
-            self._handle_io(reading=False)
-            if cli_queue.qsize() > 0:
 
-                command = cli_queue.get()
-                if command == self.EXIT_COMMAND:
+            self.handle_io_lock.acquire()
+            read_out = self._handle_io(reading=False)                   
+            self.handle_io_lock.release()
+
+            cli_queue.put(read_out)
+    
+            if cli_queue.qsize() > 0:   
+                self.buffer = cli_queue.get()
+                if self.buffer == self.EXIT_COMMAND:
                     break
-                client_socket.sendall(bytes(command, encoding='utf8'))
+                client_socket.sendall(bytes(self.buffer, encoding='utf8'))
                 
 
     def start(self):
         self.server.listen()
 
         print(f'[+] Server [{self.host}:{self.port}] listening...')
-
         client_socket, client_addr = self.server.accept()
+        print(f'[+] Connected to [{client_addr[0]}:{client_addr[1]}] ^-^ ')
 
-        stop_threads = False
-
-        # TODO multithreading or multiprocessing ?
         read_thread = threading.Thread(
-            target = self._read_client, args = (lambda: stop_threads, client_socket,))
+            target = self._read_client, args = (client_socket,))
         write_thread = threading.Thread(
-            target = self._write_client, args = (lambda: stop_threads, client_socket,))
+            target = self._write_client, args = (client_socket,))
 
         self.threads.append(read_thread)
         read_thread.start()
@@ -76,29 +79,25 @@ class Server:
         write_thread.start()
 
         write_thread.join()
-        #read_thread.join()
-        stop_threads = True
+        read_thread.join()
+        
 
     def _force_stop_threads(self):
-        print()
-        for thread in self.threads:
-            # TODO stop them
-            print(f'{thread.name} alive: {thread.is_alive()}')
-            """
+        print(f'[+] Stop threads...')
+        for thread in self.threads:            
             if (thread.is_alive()):
-                thread._stop_event.set()
-            """
-            print(f'{thread.name} alive: {thread.is_alive()}')
+                time.sleep(self.time_out)      
+            print(f'{thread.name} {type(thread)} alive: {thread.is_alive()}')
     def stop(self):
         self.server.close()
         self._force_stop_threads()
         print(f'[+] Server stopped succesfully...')
     
-server = Server('192.168.43.88', 4444)
+server = Server('192.168.1.147', 4444)
 
 try:
     server.start()
 except Exception:
-    pass
+    print(f'[-] Server closed with exception')
 server.stop()
 exit(0)
